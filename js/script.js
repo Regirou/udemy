03bade52b5f29bed28672ac07811a55bd4b3b365
Regirou <regirou82@gmail.com>
2021-03-15 17:17:10 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// alert('Hello');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = +prompt('Are you already 18?', '18');
+// console.log(answer+5);
+
+// const answers = [];
+
+// answers[0] = prompt("What's your first name?", '');
+// answers[1] = prompt("What's your last name?", '');
+// answers[2] = prompt("What age are you?", '');
+
+
+// document.write(answers);
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Regina';
+alert(`Hi, ${user}`);
