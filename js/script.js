71854c271cac315311386abf8140cb325ca16fff
Regina <regirou82@gmail.com>
2021-03-16 11:20:32 +0100

    Statrted to write a program

--- js/script.js
+++ js/script.js
@@ -17,8 +17,51 @@
 
 // document.write(answers);
 
-const category = 'toys';
-console.log(`https://someurl.com/${category}/5`);
+// const category = 'toys';
+// console.log(`https://someurl.com/${category}/5`);
 
-const user = 'Regina';
-alert(`Hi, ${user}`);
+// const user = 'Regina';
+// alert(`Hi, ${user}`);
+
+
+// console.log('arr' + "-object");
+// console.log(4 + "5");
+
+// let incr = 10,
+//     decr = 10;
+
+// // ++incr;
+// // --decr;
+
+// console.log(++incr);
+// console.log(--decr);
+
+// console.log(11%4);
+
+// console.log(2*4 == 8);
+// console.log(2*4 === '8');
+
+// const isChecked = false,
+//       isClosed = false;
+    
+// console.log(isChecked || !isClosed);      
+
+const numberOfFilms = +prompt ('How many movies have you already watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt ('What was your last movie seen?', ''),
+      b = prompt ('what rate whould you give it?', ''),
+      c = prompt ('What was your last movie seen?', ''),
+      d = prompt ('what rate whould you give it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
